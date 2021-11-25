249ba864fbcc73f8074aac151b63cfb5eeb90daf
Tony Bowden <tony@tmtm.com>
2021-11-25 19:09:55 +0200

    Skip junior positions for now

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,19 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+# Skip junior ministers for now
+# TODO: make sure all are included
+class Comparison < EveryPoliticianScraper::Comparison
+  SKIP = /^(Vice Minister|Secretary of State|State Secretary)/
+
+  def wikidata
+    @wikidata ||= super.delete_if { |row| row[:position] =~ SKIP }
+  end
+
+  def external
+    @external ||= super.delete_if { |row| row[:position] =~ SKIP }
+  end
+end
+
+diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
